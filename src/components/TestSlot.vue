9f0981275a6c6bf80013a376fdb6f525131bc40a
marillo <519374754@qq.com>
2018-04-16 11:31:20 +0800

    学习slot

---
+++ src/components/TestSlot.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <h1>我是嘻嘻嘻嘻</h1>
+    <slot>只有在没有要分发的内容时才显示</slot>
+    <slot any="我是作用域插槽"></slot>
+    <header>
+      <slot name="header"></slot>
+    </header>
+    <ul>
+      <slot name="item" v-for="item in items" :text="item.text">
+      <!-- 备用信息 -->
+      嘎嘎嘎啊嘎嘎
+      </slot>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestSlot',
+  props: ['items'],
+  // data() {
+  //   return {
+  //     items: [
+  //       {
+  //         text: '111',
+  //         name: 'yyl',
+  //       },
+  //       {
+  //         text: '222',
+  //         name: 'yyl',
+  //       },
+  //       {
+  //         text: '333',
+  //         name: 'yyl',
+  //       },
+  //     ],
+  //   };
+  // },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,48 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!-- <img src="../assets/logo.png"> -->
+    <test-slot :items="items">
+      <p slot-scope="{ any }">{{ any }}</p>
+      <p slot="header">我是具名插槽</p>
+      <li slot="item" slot-scope="props">{{props.text}}</li>
+    </test-slot>
+    <HelloWorld @hello-click="changeMsg" :msg="newHello"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import TestSlot from '@/components/TestSlot.vue';
 import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'home',
+  data() {
+    return {
+      newHello: 'hi guy!',
+      items: [
+        {
+          text: '111',
+          name: 'yyl',
+        },
+        {
+          text: '222',
+          name: 'yyl',
+        },
+        {
+          text: '333',
+          name: 'yyl',
+        },
+      ],
+    };
+  },
+  methods: {
+    changeMsg() {
+      this.newHello = 'xxx';
+    },
+  },
   components: {
+    TestSlot,
     HelloWorld,
   },
 };
